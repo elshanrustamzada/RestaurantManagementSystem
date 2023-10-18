1b1ed7b84eba39ea044cefc771276d0754eb46e9
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-18 19:39:47 +0400

    add gentelaAdminPanel

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantManagementSystem.Models;
+using System.Diagnostics;
+
+namespace RestaurantManagementSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+       
+    }
+}
--- RestaurantManagementSystem/RestaurantManagementSystem/Models/AppUser.cs
+++ RestaurantManagementSystem/RestaurantManagementSystem/Models/AppUser.cs
@@ -4,8 +4,8 @@ namespace RestaurantManagementSystem.Models
 {
     public class AppUser : IdentityUser
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
         public bool IsDeactive { get; set; }
     }
 }
