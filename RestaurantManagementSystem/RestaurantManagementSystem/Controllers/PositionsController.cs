cf428e3a543a331a571f676a2aaedbffce583a39
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-11-02 19:37:12 +0400

    add Create and Update get and post methods

--- RestaurantManagementSystem/RestaurantManagementSystem/Controllers/PositionsController.cs
+++ RestaurantManagementSystem/RestaurantManagementSystem/Controllers/PositionsController.cs
@@ -6,7 +6,7 @@ using RestaurantManagementSystem.Models;
 
 namespace RestaurantManagementSystem.Controllers
 {
-    [Authorize(Roles ="Admin,Member")]
+    [Authorize(Roles = "Admin,Member")]
     public class PositionsController : Controller
     {
         private readonly AppDbContext _db;
@@ -19,5 +19,73 @@ namespace RestaurantManagementSystem.Controllers
             List<Position> positions = await _db.Positions.ToListAsync();
             return View(positions);
         }
+
+        #region Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Position position)
+        {
+            #region Is Exist
+            bool isExist = await _db.Positions.AnyAsync(x => x.Name == position.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "Bu vəzifə artıq mövcuddur!");
+                return View();
+            }
+            #endregion
+
+            await _db.Positions.AddAsync(position);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Update
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Position? dbPosition = await _db.Positions.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbPosition == null)
+            {
+                return BadRequest();
+            }
+            return View(dbPosition);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Position position)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Position? dbPosition = await _db.Positions.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbPosition == null)
+            {
+                return BadRequest();
+            }
+
+            #region Is Exist
+            bool isExist = await _db.Positions.AnyAsync(x => x.Name == position.Name && x.Id != id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "Bu vəzifə artıq mövcuddur!");
+                return View();
+            }
+            #endregion
+
+            dbPosition.Name = position.Name;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
     }
 }
